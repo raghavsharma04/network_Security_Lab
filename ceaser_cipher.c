6e1728f5d6d0b61fdd81b8e5b81e9b5efe07090b
raghav0477 <sharmaraghav0412@gmail.com>
2022-09-29 21:26:53 +0530

    a

---
+++ ceaser_cipher.c
@@ -0,0 +1,48 @@
+//Program to Perform Ecryption and Decryption using Ceaser Cipher
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char message[100], ch;
+    int i, key;
+    printf("Enter the message to Encrypt ");
+    gets(message);
+    printf("Enter key");
+    scanf("%d", &key);
+    for ( i = 0; message[i]!='\0'; i++)
+    {
+        ch = message[i];
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch + key;
+            message[i] = ch;
+        }
+        else if(ch>='A' && ch <= 'Z'){
+            ch = ch+ key;
+            message[i] = ch;
+        }
+    }
+    printf("Encrypted Message: %s", message);
+    for ( i = 0; message[i] != '\0'; ++i)
+    {
+        ch = message[i];
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch - key;
+            if ((ch<'a'))
+            {
+                ch = ch + 'z' -'a' + 1;
+            }
+            message[i] = ch;
+        }
+        else if(ch>='A' && ch <= 'Z'){
+          ch = ch - key;
+            if ((ch<'A'))
+            {
+                ch = ch + 'Z' -'A' + 1;
+            }
+            message[i] = ch;
+        }
+    }
+    printf("\n Decrypted message: %s", message);
+    return 0;
+}
